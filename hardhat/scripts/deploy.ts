b7a9839f96cf3cd9363013412559def8fb8b180e
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-07-30 22:14:12 +0530

    feat: deploy script

---
+++ hardhat/scripts/deploy.ts
@@ -0,0 +1,36 @@
+import { ethers } from "hardhat";
+import { products } from "../constants/items.json";
+import { tokens } from "../utils";
+
+const main = async () => {
+  // SetUp Account
+  const [deployer] = await ethers.getSigners();
+
+  // Deploy contract
+  const cryptocart = await ethers.getContractFactory("CryptoCart");
+  const contract = await cryptocart.deploy();
+  const contractAddress = await contract.getAddress();
+  console.log(`Deployed Contract at => ${contractAddress}`);
+
+  // Add some product items
+  for (let i = 0; i < products.length; i++) {
+    const tx = await contract.createProduct(
+      products[i].id,
+      products[i].name,
+      products[i].category,
+      products[i].image,
+      tokens(+products[i].price),
+      products[i].rating,
+      products[i].stock
+    );
+    await tx.wait();
+    console.log(`Listed item ${products[i].id}: ${products[i].name}`);
+  }
+};
+
+main()
+  .then(() => console.log("Deploy successful !!"))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
